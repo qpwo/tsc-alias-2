0b93dfcb73bd1e3ecc2ed8974ffd582f7bde3ad0
Luke Harold Miles <lukem@sent.com>
2022-05-06 13:09:23 -0700

    add middle import to example

--- src/nested/directory/b.ts
+++ src/nested/directory/b.ts
@@ -1,5 +1,8 @@
 import { sum } from 'lodash-es'
-
+// import { sum } from '../../../my-lodash-build.mjs'
+// import sum from '../../../mymap.mjs'
+import { middleVal } from '../middle'
+console.log({ middleVal })
 export const y = sum([1, 2, 3])
 
 export function foo() {
